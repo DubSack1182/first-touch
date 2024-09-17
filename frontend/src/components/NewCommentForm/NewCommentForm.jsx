ba68fcb2233ff9609deac39fe2510f9c702987d3
kevindbln@gmail.com <kevindbln@gmail.com>
2024-09-17 00:27:10 -0400

    Components Comment Complete

---
+++ frontend/src/components/NewCommentForm/NewCommentForm.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import * as touchService from '../../services/touchesService';
+
+const CommentForm = (props) => {
+  const [formData, setFormData] = useState({ text: '' });
+  const { touchId, commentId } = useParams();
+  const navigate = useNavigate();
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (touchId && commentId) {
+        touchService.updateComment(touchId, commentId, formData);
+        navigate(`/touches/${touchId}`);
+      } else {
+        props.handleAddComment(formData);
+      }
+      setFormData({ text: '' });
+  };
+
+  useEffect(() => {
+    const fetchTouch = async () => {
+      const touchData = await touchService.show(touchId);
+      // Find comment in fetched touch data
+      setFormData(touchData.comments.find((comment) => comment._id === commentId));
+    };
+    if (touchId && commentId) fetchTouch();
+  }, [touchId, commentId]);
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="text-input">Your comment:</label>
+      <textarea
+        required
+        type="text"
+        name="text"
+        id="text-input"
+        value={formData.text}
+        onChange={handleChange}
+      />
+      <button type="submit">SUBMIT COMMENT</button>
+    </form>
+  );
+};
+
+export default CommentForm;
